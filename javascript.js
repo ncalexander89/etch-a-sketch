37b5311ae4fa9910848a4017cf8cf80f60c1ef14
Chris <nc.alexander89@gmail.com>
2022-03-01 13:59:29 +1100

    rainbow/pass over function

--- javascript.js
+++ javascript.js
@@ -1,29 +1,49 @@
 const grid = document.querySelector('#grid');
 let sq = 3;
-//functions
+
+//functions number of squares
 function run (sq) {
-for (i=0;i<(sq*sq);i++) {
-    const squares = document.createElement('div');
-    squares.classList.add('squares');
-    grid.appendChild(squares);
-    squares.setAttribute('style', 'color: blue; background: white'); 
+    for (i=0;i<(sq*sq);i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        grid.appendChild(squares);
+        squares.setAttribute('style', 'color: blue; background: white'); 
+    }
 }
-}
+
+//mouse over function
+let count = 0;
 window.addEventListener('mouseover', function(color) {
         if (color.target.className==='squares'){
-        color.target.style.backgroundColor = "green";
+            count += 1; // squares completely black after 1000 pass overs
+            const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+            let rN = randomBetween(0, 255);
+            let r = rN - (rN)/(1000/`${count}`);
+            let gN = randomBetween(0, 255);
+            let g = gN - (gN)/(1000/`${count}`);
+            let bN = randomBetween(0, 255);
+            let b = bN - (bN)/(1000/`${count}`);
+            let rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
+            color.target.style.backgroundColor = rgb;
         }
-        window.addEventListener('click', function(e) {
+// clear function
+window.addEventListener('click', function(e) {
             if (e.target.className==='clear'){
             color.target.style.backgroundColor = "";
+            count = 0;
             }
         });
-})
+    })
+
 run(sq);
 //user input number of squares per side
 function promptFunction(sq) {
     deleteChild();
     sq = prompt('# SQ'); 
+    if(sq>64) {
+        alert('must be less than 64');
+        sq = prompt('# SQ'); 
+    }
     run(sq);  
 //updates flex basis through DOM
     const array = document.getElementsByClassName("squares");
